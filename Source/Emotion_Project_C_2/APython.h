9f1b31717e272190cb93cd009ee2dfb4c7e9776a
BP-norn <ryo502045@gmail.com>
2018-12-05 00:45:33 +0900

    C++

---
+++ Source/Emotion_Project_C_2/APython.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PyUserWidget.h"
+#include "APython.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMOTION_PROJECT_C_2_API UAPython : public UPyUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
