1b11b354186d19eafacaf14c691a3db645755527
Su2uY0u <43472441+Su2uY0u@users.noreply.github.com>
2018-12-14 18:50:33 +0900

    C++

--- Content/Scripts/cmd.py
+++ Content/Scripts/cmd.py
@@ -1,7 +1,3 @@
-#import subprocess
 import unreal_engine as ue
 
-#cmd = 'ue.py_exec(ue.find_plugin('UnrealEnginePython').get_base_dir() + '/write.py')'
-ue.py_exec(ue.find_plugin('UnrealEnginePython').get_base_dir() + '/write.py')
-
-#subprocess.Popen(cmd)
+ue.py_exec(ue.find_plugin('UnrealEnginePython').get_base_dir() + '/Emotion/write.py')
